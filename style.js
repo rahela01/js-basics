4531a0b5213700047ebf0c6b9928cf12fa479929
rahela01 <rahelatabaassum102@gmail.com>
2020-03-02 20:02:17 -0800

    completed array

---
+++ style.js
@@ -0,0 +1,21 @@
+// array
+var a = [8,3,5,7,1]
+a.push(6,9,8);
+console.log(a);
+a.pop(3);
+console.log(a);
+a.sort();
+console.log(a);
+a.shift(3);
+console.log(a);
+a.unshift(5);
+console.log(a);
+a.reverse() ;
+console.log(a);
+// array
+var array = [1,2,3,4,5]
+console.log(array = [1,2,3,4,5])
+function arr(){
+    for( i=0;i<=5;i++);
+    console.log(arr)
+}
