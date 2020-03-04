28a92ee59c172b83ff86c4f6fcceb987cfef9726
rahela01 <rahelatabaassum102@gmail.com>
2020-03-03 19:37:58 -0800

    completed array

--- style.js
+++ style.js
@@ -12,10 +12,68 @@ a.unshift(5);
 console.log(a);
 a.reverse() ;
 console.log(a);
-// array
-var array = [1,2,3,4,5]
-console.log(array = [1,2,3,4,5])
-function arr(){
-    for( i=0;i<=5;i++);
-    console.log(arr)
+
+//Array assignment
+ var array=[1,2,3,4,5,"complete"];
+ function arr(){
+     for (i=0;i<array.length;i++)
+     console.log(array[i]);
+ } arr();
+
+var name = 'John';
+
+function first() {
+	var a = 'Hello!';
+    second();
+    var x = a + name;
 }
+
+function second() {
+    var b = 'Hi!';
+    third();
+    var y = b + name
+}
+
+function third() {
+    var c = 'Hey!';
+    var z = c + name
+    console.log(z);
+    
+}
+
+first();
+
+//basics
+var a=1 ;
+console.log(a);
+ 
+//funtion
+function add(){
+    console.log('called add function');
+}
+add();
+//sub
+function sub(a,b){
+    var c=a-b;
+    return c;
+}
+var result=sub(9,5);
+console.log(result)
+
+var teacher = "Salman";
+function otherClass () {
+    teacher = "Tajammul";
+    topic = "JS"
+    console.log("Welcome!",topic); 
+}
+otherClass();
+
+//7. IIFEs
+
+(function () {
+    var aName = "Barry";
+    console.log(aName);
+    
+})();
+
+
